866092445f0599c11047281104a91c7296f3fc97
tsteiger <tsteiger@gaig.com>
2025-05-13 14:18:11 +0000

    java studies

---
+++ java-oop/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    int speed = 120;
+
+    public static void main(String[] args) {
+        Car sportsCar = new Car();
+
+        System.out.println("The speed of the sportsCar is: " + sportsCar.speed + " km/h");
+    }
+}
